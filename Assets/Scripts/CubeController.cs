b3495549cc65d679b7b4fffbe38d4999cd93a79e
bryan lee <bryanlee21@live.com>
2019-04-10 00:31:44 +0000

    Setup profiles for mrtk config

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.MixedReality.Toolkit.Core.EventDatum.Input;
+using Microsoft.MixedReality.Toolkit.SDK.Input.Handlers;
+using UnityEngine;
+public class CubeController : BaseFocusHandler
+{
+    public TextMesh textMesh;
+    public void Select_Cube()
+    {
+        textMesh.text = "User selected Cube";
+    }
+    public void Rotate_Cube()
+    {
+        textMesh.text = "User wants to rotate Cube";
+    }
+  //  public override void OnFocusEnter(FocusEventData eventData)
+   // {
+   //     textMesh.text = "User looking at cube";
+   // }
+   // public override void OnFocusExit(FocusEventData eventData)
+   // {
+   //     textMesh.text = "User not looking at cube";
+   // }
+}
