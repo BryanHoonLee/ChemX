3f96d077f16418705ad5d756953c363bf62c6700
Joshua Chen <josh526chen@gmail.com>
2019-04-10 04:51:00 +0000

    Alkaline Earth Metals Set

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -16,7 +16,87 @@ public class Spawn : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        // Alkali Metals
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Li
+            if (key == "A1" && Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Li Spawned");
+            }
+            // K
+            if (key == "A2" && Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("K Spawned");
+            }
+            // Na
+            if (key == "A3" && Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Na Spawned");
+            }
+            // Rb
+            if (key == "A4" && Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Rb Spawned");
+            }
+            // Cs
+            if (key == "A5" && Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Cs Spawned");
+            }
+            // Fr
+            if (key == "A6" && Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Fr Spawned");
+            }
+        }
+        // Alkaline Earth Metals
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Be
+            if (key == "S1" && Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Be Spawned");
+            }
+            // Mg
+            if (key == "S2" && Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Mg Spawned");
+            }
+            // Ca
+            if (key == "S3" && Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Ca Spawned");
+            }
+            // Sr
+            if (key == "S4" && Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Sr Spawned");
+            }
+            // Ba
+            if (key == "S5" && Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Ba Spawned");
+            }
+            // Ra
+            if (key == "S6" && Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                Instantiate(Element, transform.position, transform.rotation);
+                print("Ra Spawned");
+            }
+        }
         if (key == "Q" && Input.GetKeyDown(KeyCode.Q))
         {
             Instantiate(Element, transform.position, transform.rotation);
@@ -47,21 +127,7 @@ public class Spawn : MonoBehaviour
             Instantiate(Element, transform.position, transform.rotation);
             print("Noble Gas Spawned");
         }
-        if (Input.GetButtonDown("A"))
-        {
-            if (key == "A1" && Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                Instantiate(Element, transform.position, transform.rotation);
-                print("Li Spawned");
-                print("Alkali Metal Spawned");
-            }
 
-        }
-        if (key == "S" && Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(Element, transform.position, transform.rotation);
-            print("Alkali Earth Metal Spawned");
-        }
         if (key == "J" && Input.GetKeyDown(KeyCode.J))
         {
             Instantiate(Element, transform.position, transform.rotation);
