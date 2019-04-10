cad18a0e75927f5c549b179b6b3ce6604d159d52
Joshua Chen <josh526chen@gmail.com>
2019-04-10 02:05:32 +0000

    Assets

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Core.EventDatum.Input;
+using Microsoft.MixedReality.Toolkit.SDK.Input.Handlers;
+using UnityEngine;
+
+public class MenuController : BaseFocusHandler
+{
+
+    public TextMesh textMesh;
+
+    public void clickedIonicButton()
+    {
+
+        textMesh.text = "Selected Ionic Bond";
+    }
+    public void clickedCovalentButton()
+    {
+        textMesh.text = "Selected Covalent Bond";
+    }
+    public void clickedAlkalineButton()
+    {
+
+    }
+    public void clickedAlkalineEarthButton()
+    {
+
+    }
+ 
+}
