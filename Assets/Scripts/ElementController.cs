714a8616a9c4bb9919587e7bc0284081ef04c3d9
bryan lee <bryanlee21@live.com>
2019-04-12 00:03:56 +0000

    Function: Elements can be moved around

---
+++ Assets/Scripts/ElementController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Core.EventDatum.Input;
+using Microsoft.MixedReality.Toolkit.SDK.Input.Handlers;
+using UnityEngine;
+
+public class ElementController : BaseFocusHandler
+{
+
+    public TextMesh textMesh;
+    public GameObject element;
+    public GameObject spawnPoint;
+
+    public void clickedElementButton()
+    {
+        if (element.name.Equals("Cs"))
+        {
+            Instantiate(element, spawnPoint.transform.position, transform.rotation);
+            textMesh.text = "Cs Spawned";
+        }else if (element.name.Equals("Fr"))
+        {
+            Instantiate(element, spawnPoint.transform.position, transform.rotation);
+            textMesh.text = "Fr Spawned";
+        }
+        else if (element.name.Equals("Li"))
+        {
+            Instantiate(element, spawnPoint.transform.position, transform.rotation);
+            textMesh.text = "Li Spawned";
+        }
+        else if (element.name.Equals("K"))
+        {
+            Instantiate(element, spawnPoint.transform.position, transform.rotation);
+            textMesh.text = "K Spawned";
+        }
+        else if (element.name.Equals("Rb"))
+        {
+            Instantiate(element, spawnPoint.transform.position, transform.rotation);
+            textMesh.text = "Rb Spawned";
+        }
+        else if (element.name.Equals("Na"))
+        {
+            Instantiate(element, spawnPoint.transform.position, transform.rotation);
+            textMesh.text = "Na Spawned";
+        }
+    }
+
+}
