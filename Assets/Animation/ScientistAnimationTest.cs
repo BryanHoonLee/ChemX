8298fa65fa31a08981d309e4691542fdbad13381
Steven Yu Chan <stevenyuchan@ymail.com>
2019-04-26 03:46:51 +0000

    Added animations to character when idle for a period of time

--- Assets/Animation/ScientistAnimationTest.cs
+++ Assets/Animation/ScientistAnimationTest.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ScientistAnimationTest : MonoBehaviour
 {
     public Animator scientistAnimator;
+    float timer; //20 Seconds before activation of idle animation
 
     // Start is called before the first frame update
     void Start()
     {
         scientistAnimator = GetComponent<Animator>();
-    }
 
+        timer = 0;
+        
+    }
     // Update is called once per frame
     void Update()
     {
+        UpdateTime();
+
         if(Input.GetKeyDown("a"))
         {
             scientistAnimator.Play("LOSE00", -1, 0f);
@@ -23,5 +28,20 @@ public class ScientistAnimationTest : MonoBehaviour
         {
             scientistAnimator.Play("WIN00", -1, 0f);
         }
+        if (timer >= 10 || Input.GetKeyDown("c"))
+        {
+            int random = Random.Range(1, 4);
+            scientistAnimator.Play("Idle_" + random, -1, 0f);
+            timer = 0;
+            Debug.Log("playing Idle_" + random);
+        }
+
+        Debug.Log("time: " + timer);
     }
+
+    public void UpdateTime()
+    {
+        timer += Time.deltaTime;
+    }
+
 }
--- Assets/Animation/ScientistBehavior.cs
+++ Assets/Animation/ScientistBehavior.cs
@@ -5,11 +5,41 @@ using UnityEngine;
 public class ScientistBehavior : MonoBehaviour
 {
     public Animator scientistAnimator;
+    float timer; 
+
+    const float IDLE_TIME = 20; //20 Seconds before activation of idle animation
 
     // Start is called before the first frame update
     void Start()
     {
         scientistAnimator = GetComponent<Animator>();
+
+        timer = 0;
+        
+    }
+
+    void Update()
+    {
+        if(IsIdle())
+        {
+            UpdateTime();
+        }
+        if(timer <= IDLE_TIME && IsIdle())
+        {
+            int random = Random.Range(1, 4);
+            scientistAnimator.Play("Idle_" + random, -1, 0f);
+            timer = 0;
+        }
+    }
+
+    public void UpdateTime()
+    {
+        timer += Time.deltaTime;
+    }
+
+    bool IsIdle()
+    {
+        return this.scientistAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
     }
 
     public void PlayLose()
